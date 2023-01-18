2e48471dd4ffeeda29ec37f5011e7b243c9b564d
Fouad.Hijazi <fouad.o.hijazi@gmail.com>
2023-01-18 13:22:36 +0300

    register

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,13 @@
+const db = require("./Models/db")
 const express = require("express");
 require("dotenv").config();
-const db = require("./Models/db")
-const PORT = process.env.PORT;
+const PORT =  process.env.PORT;
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 
 app.listen(PORT, () => {
   console.log(`Server is work on PORT ${PORT}`);
